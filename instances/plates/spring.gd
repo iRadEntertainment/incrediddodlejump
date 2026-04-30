5d4f5eb86b3bf96b78330559c16a50cbe070f8e3
Dario De Vita <devitadario@gmail.com>
2026-04-30 05:06:49 +0200

    feat: update spring to use atlas

--- instances/plates/spring.gd
+++ instances/plates/spring.gd
@@ -1,6 +1,8 @@
 class_name Spring
 extends StaticBody2D
 
+const REGION_SPRING_DOWN: Rect2 = Rect2(65, 455, 64, 64)
+const REGION_SPRING_UP: Rect2 = Rect2(130, 455, 64, 64)
 
 @onready var sprite: Sprite2D = %sprite
 @onready var sfx_boing: AudioStreamPlayer = %sfx_boing
@@ -8,9 +10,16 @@ extends StaticBody2D
 
 var jump_force: float = 1800.0
 
+var _atlas_tex: AtlasTexture:
+	get: return sprite.texture
+
+
+func _ready() -> void:
+	_atlas_tex.region = REGION_SPRING_DOWN
+
 
 func activate() -> void:
 	sfx_boing.play()
-	sprite.texture = load("uid://cq8me17b7rtlh")
+	_atlas_tex.region = REGION_SPRING_DOWN
 	await get_tree().create_timer(0.05).timeout
-	sprite.texture = load("uid://jtwxff37ccct")
+	_atlas_tex.region = REGION_SPRING_UP

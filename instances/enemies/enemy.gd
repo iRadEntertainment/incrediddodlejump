6a3fd8c670e324b358ace3b7169e169e0c42d4c9
Dario De Vita <devitadario@gmail.com>
2026-04-30 19:22:14 +0200

    feat: added enemy abstract and floating enemy

---
+++ instances/enemies/enemy.gd
@@ -0,0 +1,51 @@
+class_name Enemy
+extends Area2D
+
+enum Type {
+	FLY,
+	SPIKES,
+}
+
+@onready var sprite: Sprite2D = %sprite
+@onready var coll: CollisionShape2D = %coll
+
+var type: Type
+var dir: Vector2:
+	set(value):
+		if dir == value:
+			return
+		dir = value
+		sprite.flip_h = dir.x > 0
+
+var speed: float
+var velocity: Vector2
+var frames: Array
+var frames_duration: float = 0.2
+var frames_interval: float
+var current_frame_idx: int
+var _is_dead: bool
+
+
+func _ready() -> void:
+	Mng.game.enemies.register_enemy(self)
+	_setup()
+
+
+func _setup() -> void:
+	pass #NOTE: overlaod
+
+
+func die() -> void:
+	_is_dead = true
+	coll.set_deferred(&"disabled", true)
+	Mng.game.enemies.deregister_enemy(self)
+	queue_free()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		body.die(self)
+
+
+func _on_notif_screen_exited() -> void:
+	queue_free()
---
+++ instances/enemies/enemy_float.gd
@@ -0,0 +1,38 @@
+extends Enemy
+
+
+const FRAMES: Dictionary[Type, Array] = {
+	Type.FLY: [18, 19],
+	Type.SPIKES: [31, 32],
+}
+
+
+func _setup() -> void:
+	dir.x = 1 if Mng.rng.randf() > 0.5 else -1
+	
+	frames = FRAMES[type]
+	match type:
+		Type.FLY:
+			speed = 120.0
+		Type.SPIKES:
+			speed = 60.0
+
+
+func _process(delta: float) -> void:
+	frames_interval += delta
+	if frames_interval > frames_duration:
+		frames_interval = 0
+		current_frame_idx = wrapi(current_frame_idx + 1, 0, frames.size())
+		sprite.frame = frames[current_frame_idx]
+
+
+func _physics_process(delta: float) -> void:
+	_process_bounce_left_right()
+	velocity = speed * dir
+	position += velocity * delta
+
+
+func _process_bounce_left_right() -> void:
+	if dir.x > 0.0 and global_position.x > Mng.viewport_half_size.x or \
+			dir.x < 0.0 and global_position.x < -Mng.viewport_half_size.x:
+		dir.x = -dir.x

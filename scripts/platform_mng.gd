599b3e11ab30bcd26c105edf7fbd06a501effbbf
Dario De Vita <devitadario@gmail.com>
2026-04-30 22:12:59 +0200

    feat: better platform spawn handling based on difficulty

--- scripts/platform_mng.gd
+++ scripts/platform_mng.gd
@@ -3,8 +3,8 @@ extends Node2D
 
 const OFF_SCREEN_GEN_HEIGHT: float = float(Mng.PLATFORM_GRID_SIZE.y * 2)
 
-#@export var noise: FastNoiseLite
 @export_range(0, 8, 1) var max_skippable_platforms: int = 4
+
 
 var game: Game:
 	get: return Mng.game
@@ -35,15 +35,12 @@ func clear() -> void:
 
 
 func generate_platforms() -> void:
-	#if not noise:
-		#printerr("No noise assigned to the platform manager.")
-		#return
-	
-	var spawn_treshold_difficulty: float = 0.5
+	# probability to skip
+	var pr_skip: float = lerpf(0.3, 0.85, Mng.game.current_difficulty)
 	
 	for line: int in _lines_to_generate:
 		# difficulty spawn
-		var rng_skip: bool = Mng.rng.randf() > spawn_treshold_difficulty
+		var rng_skip: bool = Mng.rng.randf() < pr_skip
 		var can_skip: bool = _skipped_platform_count < max_skippable_platforms
 		var skip_platform: bool = rng_skip and can_skip
 		
@@ -51,31 +48,32 @@ func generate_platforms() -> void:
 			_skipped_platform_count += 1
 			continue
 		
-		# difficulty modifiers
-		var is_moving: bool = Mng.rng.randf() > 0.8
-		var is_disappear: bool = Mng.rng.randf() > 0.9
-		var is_breakable: bool = Mng.rng.randf() > 0.9 and can_skip and not is_disappear
-		var is_long: bool = Mng.rng.randf() > 0.6
-		var has_spring: bool = Mng.rng.randf() > 0.9 and not is_breakable
-		var has_boost: bool = false and not is_breakable # TODO
+		# difficulty probabilities modifiers
+		var pr_is_moving: float = lerpf(0.0, 0.65, Mng.game.current_difficulty)
+		var pr_is_disappear: float = lerpf(0.0, 0.3, Mng.game.current_difficulty)
+		var pr_is_breakable: float = lerpf(0.2, 0.5, Mng.game.current_difficulty)
+		var pr_is_long: float = lerpf(0.4, 0.05, Mng.game.current_difficulty)
+		var pr_has_spring: float = lerpf(0.03, 0.15, Mng.game.current_difficulty)
+		var pr_has_boost: float = lerpf(0.02, 0.04, Mng.game.current_difficulty)
 		
-		if is_breakable:
-			_skipped_platform_count += 1
-		else:
-			_skipped_platform_count = 0
-		
-		
+		# add platform
 		var line_height: float = _generated_height + line * Mng.PLATFORM_GRID_SIZE.y
 		var platform: Platform = load("uid://bnnasjoucdkug").instantiate()
 		platform.position.y = -line_height
 		platform.position.x = (Mng.rng.randf() * 2.0 - 1.0) * Mng.viewport_half_size.x
-		platform.is_moving = is_moving
-		platform.is_breakable = is_breakable
-		platform.is_disappear = is_disappear
-		platform.is_long = is_long
-		platform.has_spring = has_spring
-		platform.has_boost = has_boost
+		platform.is_moving = Mng.rng.randf() < pr_is_moving
+		platform.is_disappear = Mng.rng.randf() < pr_is_disappear
+		platform.is_breakable = (Mng.rng.randf() < pr_is_breakable) and can_skip and not platform.is_disappear
+		platform.is_long = Mng.rng.randf() < pr_is_long
+		platform.has_spring = (Mng.rng.randf() < pr_has_spring) and not platform.is_breakable
+		platform.has_boost = (Mng.rng.randf() < pr_has_boost) and not platform.is_breakable
 		add_child(platform)
+		
+		# count skipped platforms (breakable)
+		if platform.is_breakable:
+			_skipped_platform_count += 1
+		else:
+			_skipped_platform_count = 0
 	
 	_generated_height = _gen_target_height
 

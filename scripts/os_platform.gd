0ed56fb03764ac950aed1157bbdebbb8b3bac484
Dario De Vita <devitadario@gmail.com>
2026-05-01 05:44:35 +0200

    feat: os_platform detect hosts

--- scripts/os_platform.gd
+++ scripts/os_platform.gd
@@ -2,25 +2,44 @@
 class_name OSPlatform
 
 enum Type {
-	DESKTOP,
+	WINDOWS,
+	MACOS,
+	LINUX,
 	ANDROID,
 	IOS,
-	WEB,
 	WEB_ANDROID,
-	WEB_IOS
+	WEB_IOS,
+	OTHER,
+}
+enum Host {
+	PC,
+	MOBILE,
+	WEB,
+	OTHER,
 }
 
-static var platform_type: Type
+static var type: Type
+static var host: Host
 
 
 static func _static_init() -> void:
-	platform_type = _detect_platform()
+	type = _detect_platform()
+	host = _detect_host()
 
 
 static func _detect_platform() -> Type:
+	if OS.has_feature("windows"): return Type.WINDOWS
+	if OS.has_feature("macos"): return Type.MACOS
+	if OS.has_feature("linux"): return Type.LINUX
 	if OS.has_feature("web_android"): return Type.WEB_ANDROID
 	if OS.has_feature("web_ios"): return Type.WEB_IOS
 	if OS.has_feature("android"): return Type.ANDROID
 	if OS.has_feature("ios"): return Type.IOS
-	if OS.has_feature("web"): return Type.WEB
-	return Type.DESKTOP
+	return Type.OTHER
+
+
+static func _detect_host() -> Host:
+	if OS.has_feature("pc"): return Host.PC
+	if OS.has_feature("mobile"): return Host.MOBILE
+	if OS.has_feature("web"): return Host.WEB
+	return Host.OTHER

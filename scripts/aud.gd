08ab39c4011e6b6e966dba78d94e308c28ea2514
Dario De Vita <devitadario@gmail.com>
2026-05-03 00:56:30 +0200

    feat: added user volume settings and personal bests

--- scripts/aud.gd
+++ scripts/aud.gd
@@ -8,6 +8,41 @@ extends Node
 
 var _tw_theme_music: Tween
 
+#region Volumes
+var vol_master: float:
+	get:
+		return get_bus_volume(0)
+	set(value):
+		vol_master = value
+		set_bus_volume(0, value)
+var vol_sfx: float:
+	get:
+		return get_bus_volume(1)
+	set(value):
+		vol_sfx = value
+		set_bus_volume(1, value)
+var vol_music: float:
+	get:
+		return get_bus_volume(2)
+	set(value):
+		vol_music = value
+		set_bus_volume(2, value)
+var vol_ui: float:
+	get:
+		return get_bus_volume(3)
+	set(value):
+		vol_ui = value
+		set_bus_volume(3, value)
+#endregion
+
+
+func _ready() -> void:
+	if Mng.user_file:
+		vol_master = Mng.user_file.get_value("Settings", "volume_master", 0.5)
+		vol_sfx = Mng.user_file.get_value("Settings", "volume_sfx", 1.0)
+		vol_music = Mng.user_file.get_value("Settings", "volume_music", 1.0)
+		vol_ui = Mng.user_file.get_value("Settings", "volume_ui", 1.0)
+
 
 #region Music
 func play_theme_music() -> void:
@@ -33,4 +68,13 @@ func play_go() -> void: sfx_go.play(); sfx_ready.stop()
 func play_womp_womp() -> void:
 	sfx_game_over.play()
 	stop_theme_music()
+#endregion
+
+
+#region Getters/setters
+func get_bus_volume(bus: int) -> float:
+	return AudioServer.get_bus_volume_linear(bus)
+
+func set_bus_volume(bus: int, linear_volume: float) -> void:
+	AudioServer.set_bus_volume_linear(bus, linear_volume)
 #endregion
--- scripts/mng.gd
+++ scripts/mng.gd
@@ -2,6 +2,7 @@
 extends Node
 
 
+#region Game modifiers
 const PLATFORM_GRID_SIZE: Vector2i = Vector2i(64, 64)
 
 const MIN_DIFFICULTY_SCORE: int = 2000
@@ -11,6 +12,7 @@ const MIN_ENEMY_SPAWN_HEIGHT: int = 300
 const MAX_ENEMY_SPAWN_HEIGHT: int = 5000
 
 const SCORE_COST_SHOOT: int = 100
+#endregion
 
 
 #region Self-registering instances
@@ -25,6 +27,13 @@ var viewport_size: Vector2
 var viewport_half_size: Vector2
 #endregion
 
+#region Save/Load
+const USER_FILE = "incrediball.cfg"
+var _user_filepath: String:
+	get: return "user://" + USER_FILE
+var user_file: ConfigFile
+#endregion
+
 #region State
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 enum State { INIT, RUNNING, GAME_OVER, PAUSED }
@@ -32,6 +41,8 @@ enum State { INIT, RUNNING, GAME_OVER, PAUSED }
 var state: State = State.INIT: set = _set_state
 var state_prev: State
 var game_seed: String
+var score_personal_best: int
+var height_personal_best: int
 
 signal state_updated(state: State)
 #endregion
@@ -41,6 +52,41 @@ func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	viewport_size = get_viewport().get_visible_rect().size
 	viewport_half_size = viewport_size * 0.5
+	load_user_info()
+
+
+func load_user_info() -> void:
+	if not FileAccess.file_exists(_user_filepath):
+		print("Mng: No user file existing.")
+		user_file = ConfigFile.new()
+		return
+	
+	user_file = ConfigFile.new()
+	var err: Error = user_file.load(_user_filepath)
+	if err != OK:
+		print("Mng: Cannot load the user file.")
+		return
+	
+	game_seed = user_file.get_value("Stats", "game_seed")
+	score_personal_best = user_file.get_value("Stats", "score_personal_best")
+	height_personal_best = user_file.get_value("Stats", "height_personal_best")
+	
+	print("Mng: user file loaded.")
+
+
+func save_user_file() -> void:
+	if not user_file:
+		user_file = ConfigFile.new()
+	user_file.set_value("Settings", "volume_master", Aud.get_bus_volume(0))
+	user_file.set_value("Settings", "volume_sfx", Aud.get_bus_volume(1))
+	user_file.set_value("Settings", "volume_music", Aud.get_bus_volume(2))
+	user_file.set_value("Settings", "volume_ui", Aud.get_bus_volume(3))
+	
+	user_file.set_value("Stats", "game_seed", game_seed)
+	user_file.set_value("Stats", "score_personal_best", score_personal_best)
+	user_file.set_value("Stats", "height_personal_best", height_personal_best)
+	
+	user_file.save(_user_filepath)
 
 
 func go_to_title() -> void:
@@ -61,6 +107,7 @@ func start_game(new_game_seed: String = "") -> void:
 
 
 func restart() -> void:
+	save_user_file()
 	rng.seed = hash(game_seed)
 	state = State.INIT
 	get_tree().reload_current_scene()
@@ -68,6 +115,7 @@ func restart() -> void:
 
 
 func quit() -> void:
+	save_user_file()
 	if not OS.has_feature("web"):
 		get_tree().quit()
 
@@ -83,6 +131,9 @@ func _set_state(new_state: State) -> void:
 	match state:
 		State.INIT: pass
 		State.RUNNING: Aud.play_go()
-		State.GAME_OVER: pass
+		State.GAME_OVER:
+			score_personal_best = max(score_personal_best, Mng.game.score)
+			height_personal_best = max(height_personal_best, Mng.game.max_height)
+			save_user_file()
 	
 	state_updated.emit(state)

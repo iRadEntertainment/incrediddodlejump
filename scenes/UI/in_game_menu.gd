be0a79692b53d819d72ccde044850a04a46a17bc
Dario De Vita <devitadario@gmail.com>
2026-05-03 04:39:25 +0200

    feat: small ui improvements

--- scenes/UI/in_game_menu.gd
+++ scenes/UI/in_game_menu.gd
@@ -1,21 +1,21 @@
 class_name InGameMenu
 extends Control
 
-@onready var btn_quit: Button = %btn_quit
+
+@onready var tabs: TabContainer = %tabs
 
 
 func _ready() -> void:
+	visibility_changed.connect(_on_visibility_changed)
 	hide()
-	btn_quit.visible = not OS.has_feature("web")
 
 
-func _on_btn_title_pressed() -> void:
-	Mng.go_to_title()
+func _on_visibility_changed() -> void:
+	if visible:
+		tabs.current_tab = 0
 
 
-func _on_btn_restart_pressed() -> void:
-	Mng.restart()
-
-
-func _on_btn_quit_pressed() -> void:
-	Mng.quit()
+func _on_btn_title_pressed() -> void: Mng.go_to_title()
+func _on_btn_restart_pressed() -> void: Mng.restart()
+func _on_btn_settings_pressed() -> void: tabs.current_tab = 1
+func _on_btn_back_pressed() -> void: tabs.current_tab = 0
